9cfd2f2edb912262f2bea01580e7e73fc801cd14
AMAN MASIH <39672588+amanmasih@users.noreply.github.com>
2022-03-07 15:56:12 +0530

    Delete MuzixApplication directory

--- MuzixApplication/trackrecommender/src/main/java/niit/trackrecommender/exception/TrackAlreadyExist.java
+++
@@ -1,8 +0,0 @@
-package niit.trackrecommender.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.CONFLICT,reason = "Track already Exists")
-public class TrackAlreadyExist extends Exception{
-}
